2da01d4807887f7d7b0e230f1beb969224929549
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2016-10-25 01:12:46 +0200

    Updating pip installer script for 3.5.x (32bit)

--- pip3_dependency_installer.bat
+++ pip3_dependency_installer.bat
@@ -1,11 +1,32 @@
-@echo off
-
+REM Upgrade PIP
 python -m pip install --upgrade pip
 
-pip3 install --upgrade numpy-stl
+REM Install cx_Freeze
+pip3 install cx_Freeze-5.0-cp35-cp35m-win32.whl
 
-pip3 install --upgrade zeroconf
+REM Install SIP
+pip3 install sip --upgrade
 
-pip3 install --upgrade PyQt5
+REM Install PYQT
+pip3 install PyQt5 --upgrade
 
+REM Install PROTOBUF
+pip3 install protobuf-3.1.0.post1-py2.py3-none-any.whl
+
+REM Install NUMPY
+pip3 install numpy-1.11.2+mkl-cp35-cp35m-win32.whl
+
+REM Install SCIPY
+pip3 install scipy-0.18.1-cp35-cp35m-win32.whl
+
+REM Install PySerial
+pip3 install pyserial --upgrade
+
+REM Install Zeroconf
+pip3 install zeroconf --upgrade
+
+REM Install NumPy-STL
+pip3 install numpy-stl --upgrade
+
+REM Install Py2Exe
 pip3 install --upgrade py2exe
